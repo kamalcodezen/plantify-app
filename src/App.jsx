20b6fb4fd688cd2e8e1dbd255ad89bc19911d39d
kamalcodezen <kamaluddin7908@gmail.com>
2026-03-19 00:28:20 +0530

    font added and banner image added and basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,33 @@ function App() {
       {/* Navbar */}
       <header className="border-b">
         <Container>
+
+
           <h1>Plantify</h1>
+
+
         </Container>
       </header>
 
       {/* Main */}
       <main className="py-8">
         <Container>
+
+
           <p>Main Content</p>
+
+
         </Container>
       </main>
 
       {/* Footer */}
       <footer className="border-t">
         <Container>
+
+
           <p>Footer</p>
+
+
         </Container>
       </footer>
     </>
